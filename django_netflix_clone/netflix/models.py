a88bd485cf79fd9d336c51b0b621ca0972e8eebd
footroot <footroot.72@gmail.com>
2022-09-04 14:20:43 +0100

    first changes netflix clone

--- django_netflix_clone/netflix/models.py
+++ django_netflix_clone/netflix/models.py
@@ -17,3 +17,9 @@ class Movie(models.Model):
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+    
+class Tag(models.Models):
+    """Tag model class."""
+    name = models.CharField(max_length=CHARS_MAX_LENGTH, blank=True)
+    description = models.TextField(blank=True, null=True)
+        
